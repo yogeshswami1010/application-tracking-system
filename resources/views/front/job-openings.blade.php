9639a8562444a0f1d422c3adc1aa5eea67a70000
yogeshswami1010 <yogeshswami1010@gmail.com>
2026-05-28 23:00:30 +0530

    hidejob location

--- resources/views/front/job-openings.blade.php
+++ resources/views/front/job-openings.blade.php
@@ -114,13 +114,13 @@
                                 @else
                                     <p class="text-[13px] font-medium mb-5 text-[#8892A0]">&nbsp;</p>
                                 @endif
-                                <div class="flex items-center justify-between pt-4 border-t border-[#F0EEE9]">
+                                <!-- <div class="flex items-center justify-between pt-4 border-t border-[#F0EEE9]">
                                     <span class="flex items-center gap-1.5 text-[13px] text-[#8892A0]">
                                         <svg class="w-3.5 h-3.5 shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"/><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"/></svg>
                                         {{ ucwords($location->location->location) }}
                                     </span>
                                     <span class="fr-badge" style="background: {{ $iconWrap[$i] }}; color: {{ $iconColor[$i] }};">{{ ucwords($location->job->category->name) }}</span>
-                                </div>
+                                </div> -->
                             </div>
                         </a>
                     </div>
