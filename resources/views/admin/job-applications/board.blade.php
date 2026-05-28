45b27feeadaba541d3c19f4d66bec0793dba30d7
yogeshswami1010 <yogeshswami1010@gmail.com>
2026-05-28 20:15:49 +0530

    Filter

--- resources/views/admin/job-applications/board.blade.php
+++ resources/views/admin/job-applications/board.blade.php
@@ -273,7 +273,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5"
                                     d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0"/>
                             </svg>
-                            <span id="ja-job-total-count">0</span>&nbsp;@lang('app.applied')
+                            <span id="ja-job-total-count">0</span>&nbsp; Total Applicant
                         </span>
                     </div>
 
