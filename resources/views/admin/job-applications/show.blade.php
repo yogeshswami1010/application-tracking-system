70aebc668cc9ad5abd9d6e898316c253d3c36928
yogeshswami1010 <yogeshswami1010@gmail.com>
2026-06-11 17:07:57 +0530

    Time format fix on application notes

--- resources/views/admin/job-applications/show.blade.php
+++ resources/views/admin/job-applications/show.blade.php
@@ -867,7 +867,9 @@
                                     <i class="fa fa-user-circle" style="font-size:14px;color:#2563EB"></i>
                                     {{ ucwords($note->user->name) }}
                                 </span>
-                                <span class="ja-note-time">{{ $note->created_at->format('d M Y, h:i A') }}</span>
+                                <span class="ja-note-time">
+                                    {{ $note->created_at->timezone('America/Toronto')->format('d M Y, h:i A') }}
+                                </span>
                             </div>
                             <p class="note-text ja-note-body">{{ ucfirst($note->note_text) }}</p>
                             <div class="note-textarea"></div>
