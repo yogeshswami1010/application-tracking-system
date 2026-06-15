9d229e32d3efd7981bef777d6618223734a47a89
yogeshswami1010 <yogeshswami1010@gmail.com>
2026-06-15 23:49:43 +0530

    AI Search

--- resources/views/sections/left-sidebar.blade.php
+++ resources/views/sections/left-sidebar.blade.php
@@ -35,12 +35,16 @@
             </a>
            
         @endif
-        <li class="{{ Request::is('admin/ai-search*') ? 'active' : '' }}">
-            <a href="{{ route('admin.ai-search') }}">
-                <i class="fa fa-magic"></i>
-                <span>AI Search</span>
+        @if(in_array("view_job_applications", $userPermissions))
+            <a href="{{ route('admin.ai-search') }}" class="ra-nav-link {{ request()->is('admin/ai-search*') ? 'on' : '' }}">
+                <span class="ra-ni" aria-hidden="true">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.8">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"/>
+                    </svg>
+                </span>
+                <span class="ra-nl">AI Search</span>
             </a>
-        </li>
+        @endif
         <div class="ra-sec-title">@lang('menu.recruitment')</div>
 
         @if(in_array("view_job_applications", $userPermissions))
